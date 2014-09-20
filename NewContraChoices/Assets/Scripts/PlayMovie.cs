bf8c399c020e2254fe0dd3cf0090af1d53cb3821
James Taylor <taylorjames9@gmail.com>
2014-09-20 16:40:22 -0500

    Now with proper slides in place

---
+++ NewContraChoices/Assets/Scripts/PlayMovie.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayMovie : MonoBehaviour {
+
+	void Start(){
+
+				MovieTexture movie = renderer.material.mainTexture as MovieTexture;
+				movie.Play ();
+	}
+}
