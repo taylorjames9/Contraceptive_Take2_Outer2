67e812817f51d9670dceeea4dab7440fa8bbe259
James Taylor <taylorjames9@gmail.com>
2014-09-20 19:28:09 -0500

    Videos working in PreContemplation

--- NewContraChoices/Assets/Scripts/PlayMovie.cs
+++ NewContraChoices/Assets/Scripts/PlayMovie.cs
@@ -3,19 +3,27 @@ using System.Collections;
 
 public class PlayMovie : MonoBehaviour {
 
+		void Start(){
 
+
+		}
 
 		int mouseCounter;
 
 		void OnMouseDown(){
-				MovieTexture movie = renderer.material.mainTexture as MovieTexture;
-				mouseCounter++;
-				if (mouseCounter % 2 == 1) {
-						movie.Play ();
-						audio.Play ();
-				} else if (mouseCounter % 2 == 0) {
-						movie.Stop ();
-						audio.Stop ();
-				}
+				Handheld.PlayFullScreenMovie ("Patrick_MovTest.mp4", Color.black, FullScreenMovieControlMode.CancelOnInput);
+				Debug.Log ("Should be playing movie now");
+				//MovieTexture movie = renderer.material.mainTexture as MovieTexture;
+				//mouseCounter++;
+				//if (mouseCounter % 2 == 1) {
+						//movie.Play ();
+						//audio.Play ();
+						/////Handheld.PlayFullScreenMovie ("Patrick_MovTest.mp4", Color.black, FullScreenMovieControlMode.CancelOnInput);
+						//////Debug.Log ("Should be playing movie now");
+						//} else if (mouseCounter % 2 == 0) {
+						//movie.Stop ();
+						//audio.Stop ();
+						//}
+				//}
 		}
 }
